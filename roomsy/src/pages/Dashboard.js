5cbc81ef8f6089beb71bbb869369910f1d1f646a
Cortney Ngo <cortney.ngo73@yahoo.com>
2024-12-07 00:37:36 -0800

    Updated dashboard file

--- roomsy/src/pages/Dashboard.js
+++ roomsy/src/pages/Dashboard.js
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
 import '../styles/Dashboard.css';
 
-return ( 
-    <body>
-        <section className="collective">
-            <div className="calendar">
-            
-            </div>
-            <div className="dashboard"> 
-            </div>
-        </section>
-   </body>
-)
-=======
 function Dashboard() {
-    return (
-        <>
-            <div className = "dashboard">
-                <h2>Dashboard</h2>
-            </div>
-        </>
-    );
+
+    return ( 
+        <body>
+            <section className="collective">
+                <div className="calendar">
+                
+                </div>
+                <div className="dashboard"> 
+                </div>
+            </section>
+       </body>
+    )
 }
 
 export default Dashboard;
->>>>>>> 26932594df0bf2ba38142cd21493f27733abac93
